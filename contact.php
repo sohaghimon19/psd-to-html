92a81fac05259b093fc94b0709eb2e8721217f6a
sohaghimon19 <himon.sohag@gmail.com>
2019-03-10 11:10:02 +0600

    First commit

---
+++ contact.php
@@ -0,0 +1,67 @@
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="register-style.css">
+    <title>Register from</title>
+</head>
+
+<body>
+    <div class="container-fluid" id="register-container">
+           <div class="container pt-5 pb-5">
+        <div class="row">
+            <div class="col-md-6 offset-md-4">
+                <div class="register-form">
+                   <img src="images/logo.png" alt="">
+                </div>
+                <div class="form-item">
+                   <h3>send e-mail</h3>
+                    <form action="contactform.php" method="post">
+                        <div class="form-group">
+                           <input type="text" name="name" placeholder="Full name">
+                        </div>
+                         <div class="form-group">
+                           <input type="text" name="mail" placeholder="Your e-mail">
+                        </div>
+                         <div class="form-group">
+                           <input type="text" name="subject" placeholder="Subject">
+                        </div>
+                        <div class="form-group">
+                          <textarea name="massage" placeholder="Massage"></textarea>
+                        </div>
+                        <button name="submit" type="submit" class="btn btn">SEND MAIL</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+        $('.classname').scrolly({
+            bgParallax: true
+        });
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
